c1362dbcf3904749f5a61386ea4295d12e38e176
Cindy Tong <tongcindyy@gmail.com>
2020-02-23 18:51:19 -0500

    Fix passport to make /current return json for current user

--- config/passport.js
+++ config/passport.js
@@ -1,5 +1,5 @@
-const JwtStrategy = require('passport-jwt').Strategy;
-const ExtractJwt = require('passport-jwt').ExtractJwt;
+const JwtStrategy = require("passport-jwt").Strategy;
+const ExtractJwt = require("passport-jwt").ExtractJwt;
 const mongoose = require('mongoose');
 const User = mongoose.model('users');
 const keys = require('../config/keys');
@@ -9,9 +9,18 @@ options.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
 options.secretOrKey = keys.secretOrKey;
 
 module.exports = passport => {
-    passport.use(new JwtStrategy(options, (jwt_payload, done) => {
-        // This payload includes the items we specified earlier
-        console.log(jwt_payload);
-        done(); // middleware passes on to next middleware 
-    }))
+  passport.use(
+    new JwtStrategy(options, (jwt_payload, done) => {
+      User.findById(jwt_payload.id)
+        .then(user => {
+          if (user) {
+            // return the user to the frontend
+            return done(null, user);
+          }
+          // return false since there is no user
+          return done(null, false);
+        })
+        .catch(err => console.log(err));
+    })
+  );
 };
